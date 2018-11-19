efba370063843b1715333c9ac3317d92da4b58bd
Dmytro Shtanko <d.shtanko@expertmusic.net>
2018-11-19 23:21:10 +0200

    Deleted unusfull methods

--- engine/common/setText.js
+++ engine/common/setText.js
@@ -1,12 +1,6 @@
 import {client} from "nightwatch-cucumber"
 
 export class Text{
-    static async set(selector){
-        return setText(selector)
-    }
-    static async list(selector){
-        return setAttributes(selector)
-    }
     static async setValueField(selector){
         let text = null;
         await client.expect.element(selector).to.be.visible;
@@ -16,27 +10,4 @@ export class Text{
         console.log(` ✔  Set text is: ${text}`);
         return text;
     }
-}
-
-
-async function setText(selector) {
-    let text = null;
-    await client.expect.element(selector).to.be.visible;
-    await client.getText(selector, async res => {
-        text= res.value
-    });
-    console.log(` ✔  Set text is: ${text}`);
-    return text;
-}
-
-async function setAttributes(selector) {
-    let text = [];
-    await client.elements('css selector', selector, function (result) {
-        for (let i = 0; i < result.value.length; i++) {
-            this.elementIdAttribute(result.value[i].ELEMENT, `innerText`, res => {
-                text.push(res.value)
-            })
-        }
-    });
-    return text;
 }
